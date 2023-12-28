00d8750ba33aa7d107aa2957dfc21869306a5083
PaigeSylvan <paigesylvan@gmail.com>
2023-12-28 17:23:48 -0600

    wip

--- api/server.js
+++ api/server.js
@@ -20,11 +20,16 @@ server.get('/api/users', (req, res) => {
 server.get('/api/users/:id', (req, res) => {
     User.findById(req.params.id)
         .then(user => {
+            if (!user) {
+                res.status(404).json({ 
+                    message: "The user with the specified ID does not exist",
+                })
+            }
             res.json(user)
         })
         .catch(err => {
             res.status(500).json({
-                message: "error getting users",
+                message: "error getting user",
                 err: err.message,
                 stack: err.stack,
             })
